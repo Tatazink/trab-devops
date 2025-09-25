7a850210e3edc7a5a7417809f0377f786578a6e6
Thais Zink <tzink98@gmail.com>
2025-09-24 23:36:46 -0300

    ci(test): alinhar main, workflow e pytest.ini p/ S2

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ app = FastAPI()
 # 127.0.0.1:8000/
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "ok"}
 
 # 127.0.0.1:8000/teste1
 @app.get("/teste1")
 async def funcaoteste():
-    return {"teste": "deu certo"}
-
+    return {"status": "running"}
