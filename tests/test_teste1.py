66a339c991bab7f3e542b39ea0662f5a062a76c1
Thais Zink <tzink98@gmail.com>
2025-09-24 23:44:46 -0300

    test: add extra tests to meet >=5 requirement

--- tests/test_teste1.py
+++ tests/test_teste1.py
@@ -7,3 +7,8 @@ def test_teste1_ok():
     r = client.get("/teste1")
     assert r.status_code == 200
     assert r.json().get("status") == "running"
+
+def test_teste1_content_type():
+    r = client.get("/teste1")
+    assert r.headers["content-type"].startswith("application/json")
+
